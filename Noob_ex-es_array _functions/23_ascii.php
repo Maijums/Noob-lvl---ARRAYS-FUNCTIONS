34996f24cf5856fd7e6f26ec54a7852760d5e73f
Maija <97105846+Maijums@users.noreply.github.com>
2022-07-18 13:49:16 +0300

    Update 23_ascii.php

--- Noob_ex-es_array _functions/23_ascii.php
+++ Noob_ex-es_array _functions/23_ascii.php
@@ -36,27 +36,3 @@ echo shiftLetters("abcxyz") . "\n";
 
 
 
-
-/*
-
-function shift($string, $distance) {
-    $distance = $distance % 26;
-    $string = strtolower($string);
-    $result = array();
-    $characters = str_split($string);
-
-    if ($distance < 0) {
-        $distance += 26;
-    }
-
-    foreach ($characters as $idx => $char) {
-        $result[$idx] = chr(97 + (ord($char) - 97 + $distance) % 26);
-    }
-
-    return join("", $result);
-}
-
-
-*/
-
-
